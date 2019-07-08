ec1cec15b77c0d5807e18860181114d1c6f31e7c
Erin Gurley <erinmgurley@gmail.com>
2019-07-08 14:14:20 -0700

    attempting to write and chain a map and filter function

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 //describe 
 describe('map array method function test', () => {
+
   it('returns mapped array awith same length as array', () => {
     const array = map['sky', 'hot', 'Yeti', 'green'];
     expect(array).toEqual(['Is it hot?', 'Is it green?']);
