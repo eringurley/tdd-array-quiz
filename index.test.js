865baf6f8e92fa55c64d0d783f72df0440523dcd
Erin Gurley <erinmgurley@gmail.com>
2019-07-12 09:09:20 -0700

    added semis

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-const sentenceMaker = require ('../lib/index')
+const sentenceMaker = require ('../lib/index');
 //describe 
 describe('map array method function test', () => {
 
   it('takes an array and  removes Ys and makes a sentence', () => {
     const originalArray = ['sky', 'hot', 'Yeti', 'green'];
-    const expectedArray = ['Is it hot?', 'Is it green?']
+    const expectedArray = ['Is it hot?', 'Is it green?'];
     const actualArray  = sentenceMaker(originalArray);
     expect(actualArray).toEqual(expectedArray);
   });
